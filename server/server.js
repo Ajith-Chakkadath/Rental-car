11345aaa3b1a27143ff20c9fdde42d050c67cd64
Ajith Chakkadath <130172872+Ajith-Chakkadath@users.noreply.github.com>
2023-12-27 19:05:13 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
+// const bodyParser = require('body-parser')
 
 const cors = require('cors')
 const port = process.env.PORT || 2000
 
 const dbconnection = require('./db')
-
+app.use(express.json())
+// app.use(bodyParser.json())
 app.use(cors())
 app.use('/api/cars/' , require('./Routes/carsRoute'))
+app.use('/api/users/' , require('./Routes/userRoute'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
